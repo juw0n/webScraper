224ba5970e4b555b6446e668c04f1974c12b9143
Oluwajuwon Odunitan <oduns07@gmail.com>
2022-10-05 04:50:45 +0100

    getting it

--- main.go
+++ main.go
@@ -8,13 +8,13 @@ import (
 
 func main() {
 	c := colly.NewCollector(
-		colly.AllowedDomains("en.wikipedia.org", "en.wikipedia.org/wiki/Web_scraping"),
+		colly.AllowedDomains("en.wikipedia.org"),
 	)
 
 	c.OnHTML(".mw-parser-output", func(h *colly.HTMLElement) {
 		links := h.ChildAttrs("a", "href")
 		fmt.Println(links)
-		// fmt.Println(len(links)
+		// fmt.Println(h.Text)
 	})
 	c.Visit("https://en.wikipedia.org/wiki/Web_scraping")
 }
